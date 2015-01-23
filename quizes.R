a699444a0983d67d6991c7532b1d4dfea5410ffd
petersedivec@gmail.com <petersedivec@gmail.com>
2015-01-23 15:28:04 -0800

    chickPitch

---
+++ quizes.R
@@ -0,0 +1,9 @@
+install.packages("manipulate")
+library(manipulate)
+
+myPlot <- function(s) {
+    plot(cars$dist - mean(cars$dist), cars$speed - mean(cars$speed))
+    abline(0, s)
+}
+
+manipulate(myPlot(s), s = slider(0, 2, step = 0.1))
